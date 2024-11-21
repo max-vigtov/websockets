e65b50ca98ef409c8f1744681d96d525f56640a5
Max Vigueras Tovar <mvt.2000@hotmail.com>
2024-11-20 23:01:16 -0600

    Service: Server broadcast added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
@@ -10,13 +10,25 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
+
+    const payload = JSON.stringify({
+        type: 'custom-message',
+        payload: data.toString()
+    })
+    // ws.send(JSON.stringify( payload ));
+
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(payload, { binary: false });
+        }
+      });    
   });
 
-  setInterval(() => {
-    ws.send('Hola de nuevo');
-  }, 2000)
+//   setInterval(() => {
+//     ws.send('Hola de nuevo');
+//   }, 2000)
 
-  ws.send('Hola desde el servidor');
+  //ws.send('Hola desde el servidor');
 
   ws.on('close', () => {
     console.log('Client disconnected');
